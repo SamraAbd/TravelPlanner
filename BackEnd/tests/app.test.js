79bd60e7733c75be643d008d64e937cf7897c556
SamraAbd <abdullayevasamra06@gmail.com>
2025-11-04 22:45:36 +0400

    Fix test.js  to pass CI test

--- BackEnd/tests/app.test.js
+++ BackEnd/tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   test('should respond with 200 and contain title text', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('<h1>Travel Planner Web App</h1>');
+    expect(response.text).toContain('Travel Planner');
   });
 });
