630f4c428a57bc88be88129a04cf8cbb36f89887
AsmarAlv <asmer28082005@gmail.com>
2025-12-12 07:13:14 +0400

    change in index.js

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("Backend is running!!!");
+  res.send("Backend is running!");
 });
 
 app.use("/cities", citiesRoutes);
